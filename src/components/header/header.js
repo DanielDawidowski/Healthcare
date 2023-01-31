7c6fa4e8b2cfd364a428b46d5549f7cf379d8eab
Daniel Dawidowski <dvds1987@gmail.com>
2023-01-31 19:20:51 +0100

    Add animation on scroll to header and home page

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,5 @@
 import React, { useRef } from "react";
+import { motion, useScroll } from "framer-motion";
 // import { useHistory } from "react-router-dom";
 import LogoIcon from "../../assets/Images/logo/Logo.png";
 import Menu from "../menu/menu";
@@ -6,7 +7,7 @@ import Menu from "../menu/menu";
 const Header = ({ toggleMenu, setToggleMenu, small = false }) => {
     const hamburger = useRef(null);
     // let history = useHistory();
-
+    const { scrollYProgress } = useScroll();
     return (
         <div className={`header ${small && "bg"}`}>
             <div className="header__menu container">
@@ -26,6 +27,10 @@ const Header = ({ toggleMenu, setToggleMenu, small = false }) => {
                     <span></span>
                 </button>
             </div>
+            <motion.div
+                className="progress-bar"
+                style={{ scaleX: scrollYProgress }}
+            />
         </div>
     );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useRef } from "react";
+import { motion } from "framer-motion";
 import Hero from "./Home/hero";
 import Layout from "../components/layout/layout";
 import Info from "./Home/info";
@@ -6,19 +7,84 @@ import Services from "./Home/services";
 import Support from "./Home/support";
 import News from "./Home/news";
 import Apps from "./Home/apps";
-
 import Feedback from "./Home/feedback";
 
+const cardVariants = {
+    offscreen: {
+        y: -100,
+        opacity: 0
+    },
+    onscreen: {
+        y: 0,
+        opacity: 1,
+        transition: {
+            type: "spring",
+            bounce: 0.4,
+            duration: 0.8
+        }
+    }
+};
+
 const Home = () => {
+    const scrollRef = useRef(null);
     return (
-        <Layout>
-            <Hero />
-            <Info />
-            <Services />
-            <Support />
-            <News />
-            <Apps />
-            <Feedback />
+        <Layout ref={scrollRef} style={{ overflow: "scroll" }}>
+            <motion.div
+                className="card-container"
+                initial={{ opacity: 0, y: -100 }}
+                whileInView={{ opacity: 1, y: 0, transition: { duration: 1 } }}
+                viewport={{ root: scrollRef }}
+            >
+                <Hero />
+            </motion.div>
+            <motion.div
+                className="card-container"
+                initial={{ opacity: 0, y: -100 }}
+                whileInView={{ opacity: 1, y: 0, transition: { duration: 1 } }}
+                viewport={{ root: scrollRef }}
+            >
+                <Info />
+            </motion.div>
+            <motion.div
+                className="card-container"
+                initial={{ opacity: 0, y: -100 }}
+                whileInView={{ opacity: 1, y: 0, transition: { duration: 1 } }}
+                viewport={{ root: scrollRef }}
+            >
+                <Services />
+            </motion.div>
+            <motion.div
+                className="card-container"
+                initial={{ opacity: 0, y: -100 }}
+                whileInView={{ opacity: 1, y: 0, transition: { duration: 1 } }}
+                viewport={{ root: scrollRef }}
+            >
+                <Support />
+            </motion.div>
+            <motion.div
+                className="card-container"
+                initial={{ opacity: 0, y: -100 }}
+                whileInView={{ opacity: 1, y: 0, transition: { duration: 1 } }}
+                viewport={{ root: scrollRef }}
+            >
+                <News />
+            </motion.div>
+            <motion.div
+                className="card-container"
+                initial={{ opacity: 0, y: -100 }}
+                whileInView={{ opacity: 1, y: 0, transition: { duration: 1 } }}
+                viewport={{ root: scrollRef }}
+            >
+                <Apps />
+            </motion.div>
+            <motion.div
+                className="card-container"
+                initial={{ opacity: 0, y: -100 }}
+                whileInView={{ opacity: 1, y: 0, transition: { duration: 1 } }}
+                viewport={{ root: scrollRef }}
+            >
+                <Feedback />
+            </motion.div>
         </Layout>
     );
 };
